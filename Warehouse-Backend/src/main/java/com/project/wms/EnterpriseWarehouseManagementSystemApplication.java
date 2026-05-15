193fd4501ac27dc7fceb2e2f6e3d25844e4d2f1e
ISHRAQ SAIFI <133091269+ISHRAQ01@users.noreply.github.com>
2026-05-15 19:01:35 +0530

    Update CORS configuration to allow all origins

--- Warehouse-Backend/src/main/java/com/project/wms/EnterpriseWarehouseManagementSystemApplication.java
+++ Warehouse-Backend/src/main/java/com/project/wms/EnterpriseWarehouseManagementSystemApplication.java
@@ -19,10 +19,7 @@ public class EnterpriseWarehouseManagementSystemApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:3000",
-                            "https://enterprise-warehouse-management-sys-two.vercel.app"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
